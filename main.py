fdbc50b02efef28321d0471290d3b2c3965f035d
Edoardo Arturo Cancelli <edo.canc@gmail.com>
2025-09-06 16:41:30 +0200

    removed images

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pin_game_scene
 scene_handler : sh.SceneHandler = sh.SceneHandler("pin_game")
 
 #initialize all scenes
-pin_game : pin_game_scene.PinGameScene = pin_game_scene.PinGameScene(scene_handler, True, 1920638152, 8, 8, 25, 10, False)
+pin_game : pin_game_scene.PinGameScene = pin_game_scene.PinGameScene(scene_handler, True, 1248831, 11, 11, 25, 10, False)
 scene_handler.add_scene("pin_game", pin_game)
 
 while scene_handler.run():
